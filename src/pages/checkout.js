1e2a51e02717c08ffef0c451c094fafe602bb733
sorinupwork <sorin.upwork@gmail.com>
2022-12-09 00:50:49 +0200

    add stripe

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,15 +3,36 @@ import React from "react";
 import { useSelector } from "react-redux";
 import CurrencyFormat from "react-currency-format";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
 import CheckoutProduct from "../components/CheckoutProduct";
 import Header from "../components/Header";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 
+const stripePromise = loadStripe(process.env.stripe_public_key);
+
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    // call the backend to create a checkout session...
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+
+    // redirect user/customer to stripe checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
 
   return (
     <div className="bg-gray-100">
@@ -67,6 +88,8 @@ function Checkout() {
               </h2>
 
               <button
+                onClick={createCheckoutSession}
+                role="link"
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
